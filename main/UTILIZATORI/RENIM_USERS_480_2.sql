647e30c9fe6523c1a2b9b2565535f007383d0754
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-08-18 14:22:37 +0300

    Create RENIM_USERS_480_2.sql

---
+++ main/UTILIZATORI/RENIM_USERS_480_2.sql
@@ -0,0 +1,9 @@
+        SELECT *
+        
+            FROM CIS2.RENIM
+            
+            WHERE
+            
+            DENUMIRE LIKE 'TAB%'
+            
+            AND CUIIO_VERS = 480
