01dc15b2d905a31f5ad775867cfb07b3cf2d9b72
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-08-19 10:02:18 +0300

    SQL-19_08_1 #2616

---
+++ main/UTILIZATORI/RIND_.sql
@@ -0,0 +1,16 @@
+SELECT 
+  '''' ||RIND||'''' ||' ,'  AS ID_MD
+   
+FROM CIS2.MD_RIND
+
+WHERE
+STATUT = '1'
+
+AND 
+capitol=10004 AND capitol_vers=299
+
+ AND DINAMIC IS NOT NULL
+ 
+ ORDER BY 
+ 
+ ORDINE
