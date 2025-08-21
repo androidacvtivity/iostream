e100495509301067a9927715c36335b5a6cbf2c0
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-08-21 13:38:11 +0300

    18-2

--- main/RENIM_2025/RENIM_04_02_2025/UPDATE_CAEM2.sql
+++ main/RENIM_2025/RENIM_04_02_2025/UPDATE_CAEM2.sql
@@ -1,4 +1,8 @@
+DECLARE -- ====================================================================
 
+CURSOR C IS
+
+ 
 SELECT 
 DISTINCT CUIIO,
 1058 AS CUIIO_VERS,
@@ -47,3 +51,30 @@ SELECT FC.CUIIO,
                
                GROUP BY
                CUIIO
+
+  
+ 
+            
+            --------------------------------
+            ;
+
+BEGIN -- ======================================================================
+FOR CR IN C
+LOOP
+UPDATE CIS2.RENIM SET
+--
+--DENUMIRE = CR.DENUMIRE,
+--CUATM = CR.CUATM
+--CFP = CR.CFP
+--CFOJ = CR.CFOJ,
+CAEM2 = CR.CAEM2
+--IDNO = CR.IDNO
+
+WHERE
+CUIIO = CR.CUIIO 
+AND 
+CUIIO_VERS = CR.CUIIO_VERS;
+END LOOP;
+END;
+
+---------------------------
