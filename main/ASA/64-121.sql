ef1ce560cd9e4bd41fe42b5e7ce35791feec1e64
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-09-17 15:32:29 +0300

    Create 64-121.sql

---
+++ main/ASA/64-121.sql
@@ -0,0 +1,30 @@
+--SELECT 
+--   'Nu ati completat Chestionar - Preventiv'
+--  
+--  AS REZULTAT 
+--FROM
+--(
+
+SELECT COUNT(D.COL1) AS CNUM
+FROM CIS2.VW_DATA_ALL_TEMP D
+WHERE
+  (D.PERIOADA=:PERIOADA          ) AND
+  (D.CUIIO=:CUIIO                ) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM               ) AND
+  (D.FORM_VERS=:FORM_VERS ) AND
+  (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+  (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.CAPITOL IN (1123) AND
+  D.RIND IN('A','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','1','2','3')
+
+UNION
+SELECT 0 AS CNUM
+FROM DUAL
+
+) R
+
+HAVING
+  SUM(R.CNUM)=0
