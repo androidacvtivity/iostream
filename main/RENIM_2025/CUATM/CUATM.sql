26a92893cdceb593c03ffdff1fd0f9acbe2064f3
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-10-03 11:31:59 +0300

    Update CUATM.sql

--- main/RENIM_2025/CUATM/CUATM.sql
+++ main/RENIM_2025/CUATM/CUATM.sql
@@ -2,7 +2,29 @@
 
 
 
+   SELECT 
+   
+            L.*
+            
+               
+            FROM CIS2.VW_CLS_CLASS_ITEM L INNER JOIN  (
 
-
-
-            CIS2.VW_CLS_CLASS_ITEM
+            SELECT
+            CLASS_CODE, 
+            ITEM_CODE,
+            MAX(ITEM_CODE_VERS) ITEM_CODE_VERS
+               
+            FROM CIS2.VW_CLS_CLASS_ITEM
+            
+                WHERE 
+                CLASS_CODE IN ('CUATM')
+                
+                GROUP BY
+                 CLASS_CODE, 
+                 ITEM_CODE
+                
+                ) R ON R.ITEM_CODE =  L.ITEM_CODE AND L.ITEM_CODE_VERS =  R.ITEM_CODE_VERS AND R.CLASS_CODE = L.CLASS_CODE
+                
+                
+                 WHERE 
+                 L.CLASS_CODE IN ('CUATM')
