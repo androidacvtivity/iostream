7d1b193b51de42abcef770d232fafc2f09b21b2e
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-10-31 13:46:27 +0200

    45

---
+++ main/nr.1 INOV/table_out_31_10_25/tab_3.sql
@@ -0,0 +1,13 @@
+SELECT 
+                D.CUIIO,
+                D.RIND,
+                SUM(D.COL2) AS COL2 
+                FROM CIS2.VW_DATA_ALL_FR D
+                WHERE
+                
+
+            FORM = 57 
+AND CUIIO =  65465 
+AND PERIOADA = 2013
+capitol=1092  
+AND D.CUIIO = 
