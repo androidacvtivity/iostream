dd04bbcd0dbdf25888236c8e57b7ef2cc33a278f
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-11-04 13:50:28 +0200

    48

--- main/nr.1 INOV/table_out_31_10_25/RIN_10_10.sql
+++ main/nr.1 INOV/table_out_31_10_25/RIN_10_10.sql
@@ -1,15 +1,36 @@
+
+CREATE OR REPLACE FORCE VIEW VW_1_INOV_24
+AS 
 SELECT
-     
---        COUNT (DISTINCT D.CUIIO) AS CNT 
+     D.CUIIO,
+     SUM(D.COL1) AS COL1 
+
        
       FROM 
         CIS2.VW_DATA_ALL D
       WHERE
-        D.PERIOADA IN (:pPERIOADA) AND
-        D.FORM IN (:pFORM) AND 
+        D.PERIOADA IN (2013) AND
+        D.FORM IN (48) AND 
         D.CAPITOL = 1040 AND CAPITOL_VERS = 2013 AND 
-        D.RIND LIKE '1.10%' AND
+        (D.RIND LIKE '1.10%'
+        OR
+        D.RIND LIKE '1.9%') 
+        
+        AND
         D.CAEM2 NOT LIKE 'A%'
         
-       AND NVAL(D.COL1) = 1
+
        
+GROUP BY 
+     D.CUIIO
+     
+     
+     HAVING 
+     
+     SUM(CASE WHEN D.RIND LIKE '1.10%' THEN D.COL1 ELSE 0 END ) >= 0
+     OR 
+     SUM(CASE WHEN D.RIND LIKE '1.9%' THEN D.COL1 ELSE 0 END ) >= 0
+     
+     ORDER BY 
+     D.CUIIO
+     
