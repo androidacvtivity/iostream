1d5528b574b99164cdd44ae7397fb885bfcb0afe
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-11-10 13:02:57 +0200

    57

--- main/nr.1 INOV/table_out_31_10_25/RSF_AN_ID_MD.sql
+++ main/nr.1 INOV/table_out_31_10_25/RSF_AN_ID_MD.sql
@@ -1,18 +1,18 @@
-------------------------------------
 
+-----------------------
 SELECT 
                 D.CUIIO,
                 SUM(D.COL2) AS COL2 
-                FROM CIS2.VW_DATA_ALL_FR D
+                FROM CIS2.DATA_ALL_FR D
                 WHERE
                 
 
             D.FORM = 57 
----- AND D.CUIIO =  5523 
+-- AND D.CUIIO =  5523 
 AND D.PERIOADA = 2013
 AND D.FORM_VERS = 2009
-AND D.capitol=1092  
-AND D.RIND IN ('010')
+--AND D.capitol=1092  
+AND D.ID_MD IN (62206)   --62206
 
 GROUP BY 
                 D.CUIIO
@@ -33,8 +33,8 @@ SELECT
             D.FORM = 63 
 -- AND D.CUIIO =  5523 
 AND D.PERIOADA = 2013
-AND D.capitol= 1121  
-AND D.RIND IN ('010')
+--AND D.capitol= 1121  
+AND D.ID_MD IN (61891) -- 61891
 
 GROUP BY 
                 D.CUIIO
