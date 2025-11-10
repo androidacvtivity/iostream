deae3e87d229c78459bc0af08285b0df585a8dbc
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-11-10 11:29:26 +0200

    Create tab_3_RSFPrescurtat.sql

---
+++ main/nr.1 INOV/table_out_31_10_25/tab_3_RSFPrescurtat.sql
@@ -0,0 +1,17 @@
+SELECT 
+                D.CUIIO,
+                D.RIND,
+                SUM(D.COL2) AS COL2 
+                FROM CIS2.VW_DATA_ALL_FR D
+                WHERE
+                
+
+            D.FORM = 57 
+-- AND D.CUIIO =  65465 
+AND D.PERIOADA = 2013
+AND D.capitol=1092  
+AND D.RIND IN ('010')
+
+GROUP BY 
+                D.CUIIO,
+                D.RIND
