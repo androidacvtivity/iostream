2701f31d81ef01c2fcd06825a300767f515b883c
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-12-10 11:43:05 +0200

    Update M1.sql

--- main/M1/M1.sql
+++ main/M1/M1.sql
@@ -1,84 +1,30 @@
-SELECT
-  '%='|| ROUND((SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.160') AND D.PERIOADA IN (:PERIOADA) THEN D.COL1 ELSE 0 END)*100)/  
-          SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.10') AND D.PERIOADA IN (:PERIOADA)  THEN D.COL1 ELSE 0 END)/
+ SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   R.CUATM,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM
+              ( 
+              SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
+                                  
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE 
+             FC.FORM IN (:pFORM) AND FC.STATUT <> '3') FC  LEFT JOIN CUATM_CIS C ON C.CUIIO = FC.CUIIO 
+                            INNER JOIN CIS2.RENIM R ON R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS
+             
+             WHERE 
+             C.CUIIO IS NOT NULL 
+             
+             
          
-         MAX(DD.COL1))
-
-    ||' (p/u info: Anul='||
-   
-  ROUND((SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.160') AND D.PERIOADA IN (:PERIOADA) THEN D.COL1 ELSE 0 END)*1000)/  
-         SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.10') AND D.PERIOADA IN (:PERIOADA)  THEN D.COL1 ELSE 0 END))
-  
-    ||',  Anul-1='|| MAX(DD.COL1)*1000||')'
-    
---   CAP. 2 R.160 Col.1*100/R.10 Col.1 Anul/ (R.160 Col.1/R.10 Col.1)Anul-1= [100-120]% 
-
-  AS REZULTAT
-
-FROM
-  CIS2.VW_DATA_ALL D
-  LEFT JOIN 
-  (
-  
-  
-  SELECT D.CUIIO, 
-    ROUND ((CIS2.NVAL(SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('7.40.160')  THEN D.COL1 ELSE 0 END)))/  
-    CIS2.NOZERO( SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('7.40.10')  THEN D.COL1 ELSE 0 END)),3) AS COL1
-  
- FROM
-  CIS.VW_DATA_ALL D  
-WHERE   
-      (
-      
-      D.PERIOADA IN (
-                    
-                    SELECT
-                      P.PERIOADA
-                    FROM
-                      CIS.VW_MD_PERIOADA P                     
-                    WHERE
-                      P.TRIMESTR + 4 IN (:PERIOADA) 
-                      AND P.TIP_PERIOADA IN (4)
-                      
-                      
-      )) AND                  
-  (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND  
-   FORM IN (7) AND
-   CAPITOL IN (40)
-  
- GROUP BY D.CUIIO
- 
- 
- ) DD ON D.CUIIO=DD.CUIIO
-  
-WHERE
-  (D.PERIOADA IN (:PERIOADA)         ) AND
-  (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND
-  ( :CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
-  ( :FORM = :FORM               OR :FORM = -1) AND
-  ( :FORM_VERS=:FORM_VERS       OR :FORM_VERS = -1) AND
-  ( :CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
-  ( :CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
-  ( :ID_MD=:ID_MD               OR :ID_MD = -1) AND
-  
-   FORM IN (76) AND
-   CAPITOL IN (28) AND D.NUM IN (3)
-  
-  
-HAVING
-
-    ROUND(((SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.160') AND D.PERIOADA IN (:PERIOADA) THEN D.COL1 ELSE 0 END)*100)/
-   
-CIS2.NOZERO(SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.10') AND D.PERIOADA IN (:PERIOADA)  THEN D.COL1 ELSE 0 END)))/
-   
-     CIS2.NOZERO(MAX(DD.COL1)),1)  <100
-   
-   OR
-   
-    ROUND(((SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.160') AND D.PERIOADA IN (:PERIOADA) THEN D.COL1 ELSE 0 END)*100)/
-   
-CIS2.NOZERO(SUM(CASE WHEN D.FORM||'.'||D.CAPITOL||'.'||D.RIND IN ('76.28.10') AND D.PERIOADA IN (:PERIOADA)  THEN D.COL1 ELSE 0 END)))/
-   
-     CIS2.NOZERO(MAX(DD.COL1)),1)>120
---   
---   
