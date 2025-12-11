39899000059e873b7fdc2341d912cf9e7aef5ebf
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-12-11 10:51:33 +0200

    Update M1.sql

--- main/M1/M1.sql
+++ main/M1/M1.sql
@@ -14,13 +14,13 @@
               FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
+                                  WHERE FORM IN (76) AND CUIIO_VERS <= 1065
                                   
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
              WHERE 
-             FC.FORM IN (:pFORM) AND FC.STATUT <> '3') FC  LEFT JOIN CUATM_CIS C ON C.CUIIO = FC.CUIIO 
+             FC.FORM IN (76) AND FC.STATUT <> '3') FC  LEFT JOIN CUATM_CIS C ON C.CUIIO = FC.CUIIO 
                             INNER JOIN CIS2.RENIM R ON R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS
              
              WHERE 
